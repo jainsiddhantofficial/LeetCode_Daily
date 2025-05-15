fb48433e8654b846793d8b0fb18896fb5718e4cc
Siddhant Jain <138900970+jainsiddhantofficial@users.noreply.github.com>
2025-05-15 14:15:27 +0530

    Time: 23 ms (33.88%), Space: 59.4 MB (62.03%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,25 +1,27 @@
-// Time Complexity : O(n)
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // Base case...
-        if(nums == null || nums.length < 2 || k == 0)
-            return false;
-        int i = 0;
-        // Create a Hash Set for storing previous of k elements...
-        HashSet<Integer> hset = new HashSet<Integer>();
-        // Traverse for all elements of the given array in a for loop...
-        for(int j = 0; j < nums.length; j++) {
-            // If duplicate element is present at distance less than equal to k, return true...
-            if(!hset.add(nums[j])){
-                return true;
-            }
-            // If size of the Hash Set becomes greater than k...
-            if(hset.size() >= k+1){
-                // Remove the last visited element from the set...
-                hset.remove(nums[i++]);
+// Creating a "detective set" - a set (HashSet) for storing suspects (numbers).
+        Set<Integer> set = new HashSet<>(); 
+
+        // We go through each number in the array, as if we were going through every house on the street.
+        for (int i = 0; i < nums.length; i++) {
+// "Wow! Have we seen this number before?" - we are checking whether there is a current number in our set of suspects.
+            if (set.contains(nums[i])) return true; 
+            // If yes, it means that we have two identical numbers at a distance of no more than k, we return true.
+
+            // If the number is new, add it to our "detective set".
+            set.add(nums[i]); 
+
+            // "Our detective set is too big! We need to remove the old evidence."
+// If the number of suspects in the set is greater than the allowed distance k...
+if (set.size() > k) {
+// ...we remove the oldest number from the set (the number that was k positions ago).
+                set.remove(nums[i - k]); 
             }
         }
-        // If no duplicate element is found then return false...
-        return false;
+
+        // If we walked the whole street and didn't find two identical suspects at close range,
+        // so there are no duplicates, we return false.
+        return false; 
     }
 }
