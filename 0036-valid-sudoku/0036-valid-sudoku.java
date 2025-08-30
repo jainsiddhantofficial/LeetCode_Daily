76ec314b23d4b885fe3472f634af39d5158073df
Siddhant Jain <138900970+jainsiddhantofficial@users.noreply.github.com>
2025-08-30 22:23:05 +0530

    Time: 1 ms (99.87%), Space: 44.3 MB (78.74%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,26 +1,21 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        int[] rows = new int[9];
-        int[] columns = new int[9];
-        int[] boxes = new int[9];
-        
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') continue;
-                
-                int num = board[i][j] - '1'; // Convert '1'-'9' to 0-8
-                int mask = 1 << num;         // Create bitmask for the number
-                int boxIndex = (i / 3) * 3 + j / 3;
-                
-                // Check if the number is already set in the row, column, or box
-                if ((rows[i] & mask) != 0 || (columns[j] & mask) != 0 || (boxes[boxIndex] & mask) != 0) {
-                    return false;
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    int boxIndex = (i / 3) * 3 + (j / 3);
+
+                    if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
+                        return false;
+                    }
+
+                    rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
                 }
-                
-                // Mark the number in the row, column, and box
-                rows[i] |= mask;
-                columns[j] |= mask;
-                boxes[boxIndex] |= mask;
             }
         }
         return true;
