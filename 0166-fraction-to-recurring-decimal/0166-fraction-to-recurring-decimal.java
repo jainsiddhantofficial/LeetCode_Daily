bac825418d1251e11766a940e5047c66ff785f7c
Siddhant Jain <138900970+jainsiddhantofficial@users.noreply.github.com>
2025-09-24 09:42:21 +0530

    Time: 1 ms (99.8%), Space: 40.7 MB (98.56%) - LeetHub

--- 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
@@ -1,39 +1,34 @@
 class Solution {
     public String fractionToDecimal(int numerator, int denominator) {
-        if (numerator == 0) return "0";
+        if (numerator == 0)
+            return "0";
+        
+        StringBuilder fraction = new StringBuilder();
+        if (numerator < 0 ^ denominator < 0)
+            fraction.append("-");        
 
-        StringBuilder result = new StringBuilder();
-        if (numerator < 0 ^ denominator < 0) {
-            result.append("-");
-        }
-
-        long num = Math.abs((long) numerator);
-        long denom = Math.abs((long) denominator);
-        long remainder = num % denom;
-
-        result.append(num / denom);
-
+        long dividend = Math.abs(Long.valueOf(numerator));
+        long divisor = Math.abs(Long.valueOf(denominator));
+        fraction.append(dividend / divisor);
+        long remainder = dividend % divisor;
         if (remainder == 0) {
-            return result.toString();
+            return fraction.toString();
         }
 
-        result.append(".");
-        HashMap<Long, Integer> map = new HashMap<>();
-
+        fraction.append(".");
+        Map<Long, Integer> map = new HashMap<>();
         while (remainder != 0) {
             if (map.containsKey(remainder)) {
-                result.insert(map.get(remainder), "(");
-                result.append(")");
+                fraction.insert(map.get(remainder), "(");
+                fraction.append(")");
                 break;
             }
-
-            map.put(remainder, result.length());
-
+            map.put(remainder, fraction.length());
             remainder *= 10;
-            result.append(remainder / denom);
-            remainder %= denom;
+            fraction.append(remainder / divisor);
+            remainder %= divisor;
         }
 
-        return result.toString();
+        return fraction.toString();
     }
 }
