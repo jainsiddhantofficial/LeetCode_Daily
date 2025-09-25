5bf9e5b909347934df32e215de476bf74b0a978a
Siddhant Jain <138900970+jainsiddhantofficial@users.noreply.github.com>
2025-09-25 19:23:03 +0530

    Time: 9 ms (11.07%), Space: 45.6 MB (30.46%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,25 +1,11 @@
 class Solution {
-    public int helper(int[][] dp, List<List<Integer>> tri, int i, int j, int m) {
-        if (i >= m || j > i) return 0;
-        if (i == m - 1) return tri.get(i).get(j);
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int left = helper(dp, tri, i + 1, j, m);
-        int right = helper(dp, tri, i + 1, j + 1, m);
-
-        dp[i][j] = tri.get(i).get(j) + Math.min(left, right);
-        return dp[i][j];
-    }
-
     public int minimumTotal(List<List<Integer>> tri) {
-        int m = tri.size();
-        int[][] dp = new int[m][m];
-
-     
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
-
-        return helper(dp, tri, 0, 0, m);
+        for (int i = tri.size() - 2; i >= 0; i--)
+            for (int j = 0; j < tri.get(i).size(); j++)
+                tri.get(i).set(j, tri.get(i).get(j) + Math.min(
+                    tri.get(i + 1).get(j),
+                    tri.get(i + 1).get(j + 1)
+                ));
+        return tri.get(0).get(0);
     }
 }
